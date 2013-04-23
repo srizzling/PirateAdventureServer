8534804d4d48817d5a5ae34187be95f64478e309
Sriram Venkatesh <venksriram@gmail.com>
2013-04-22 20:29:06 -0400

    Authication Set Up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthexampleWebapp::Application.config.secret_token = 'c7fa24c5663f5a359a360df82639813c32b3eb80619cf5ef2b154ed16a873b004c98d3517c60843c84f93100b968a32d4768b6b56b510ec09bdc293275d92a70'
